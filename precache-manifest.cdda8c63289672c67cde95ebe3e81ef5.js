3d91fb5627a5c7bdb43703a4e84a88e4dcb8b7b3
Lakshay Dua <lakshaydua08@gmail.com>
2020-05-22 23:52:19 +0530

    Updates

---
+++ precache-manifest.cdda8c63289672c67cde95ebe3e81ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff2e94c8844063802369f0eadb8d1de",
+    "url": "/lakshaydua08/ReactApp.git/index.html"
+  },
+  {
+    "revision": "1f0210cc0d34cebf70e1",
+    "url": "/lakshaydua08/ReactApp.git/static/css/main.e42ea2a6.chunk.css"
+  },
+  {
+    "revision": "deccdd1bb6f3d582a1f1",
+    "url": "/lakshaydua08/ReactApp.git/static/js/2.fa620cb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lakshaydua08/ReactApp.git/static/js/2.fa620cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0210cc0d34cebf70e1",
+    "url": "/lakshaydua08/ReactApp.git/static/js/main.ccfe88c6.chunk.js"
+  },
+  {
+    "revision": "0e71250244ab4a596c39",
+    "url": "/lakshaydua08/ReactApp.git/static/js/runtime-main.8b1976f9.js"
+  }
+]);
